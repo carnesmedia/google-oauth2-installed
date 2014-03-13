18524fb8cef520a3d6145ea56841ddb073613225
Amiel Martin <amiel@carnesmedia.com>
2014-03-13 15:13:55 -0700

    Initial Commit
    Generated with `bundle gem google-oauth2-installed`

---
+++ lib/google/oauth2/installed.rb
@@ -0,0 +1,9 @@
+require "google/oauth2/installed/version"
+
+module Google
+  module Oauth2
+    module Installed
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/google/oauth2/installed/version.rb
@@ -0,0 +1,7 @@
+module Google
+  module Oauth2
+    module Installed
+      VERSION = "0.0.1"
+    end
+  end
+end
